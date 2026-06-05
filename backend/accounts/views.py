d5d81c6b144b5be7a43566f7e5b25084e1e16295
Albin <albinkuttu8785@gmail.com>
2026-06-05 14:50:32 +0530

    user reg 1.0

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import RegisterSerializer
+
+
+@api_view(["POST"])
+def register(request):
+
+    serializer = RegisterSerializer(
+        data=request.data
+    )
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response({
+            "message": "User created"
+        })
+
+    return Response(serializer.errors) 
